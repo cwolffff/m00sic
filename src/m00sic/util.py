190f77b6f626a415553f416afc7aad51b51decf8
Christopher Wolff <christopher.wolff@posteo.de>
2021-04-02 22:52:18 -0700

    Add util functions

--- src/m00sic/util.py
+++ src/m00sic/util.py
@@ -1,5 +1,6 @@
 import random
-import constants
+
+from . import constants
 
 
 OFFSET_TO_INT = {"I": 0, "II": 1, "III": 2, "IV": 3, "V": 4, "VI": 5}
@@ -32,8 +33,34 @@ def get_chord(key, note, chord):
     """
     assert key in constants.NOTES_FOR_KEY, f"Invalid key: {key}."
     assert note in constants.NOTES_FOR_KEY[key], f"Note {note} not in key {key}."
-    assert chord in constants.STEPS_FOR_CHORD
+    assert chord in constants.STEPS_FOR_CHORD, f"Invalid chord: {chord}."
     return [note + i for i in constants.STEPS_FOR_CHORD[chord]]
+
+
+def get_chord_first_inversion(key, note, chord):
+    notes = get_chord(key, note, chord)
+    return notes[1:] + notes[:1]
+
+
+def get_chord_second_inversion(key, note, chord):
+    notes = get_chord(key, note, chord)
+    return notes[-1:] + notes[:-1]
+
+
+def build_chord_progression(key):
+    note1 = get_starting_note(key, "I")
+    chord1 = get_chord(key=key, note=note1, chord="major_triad")
+
+    note2 = get_starting_note(key, "V")
+    chord2 = get_chord_second_inversion(key=key, note=note2, chord="major_triad")
+
+    note3 = get_starting_note(key, "VI")
+    chord3 = get_chord_first_inversion(key=key, note=note3, chord="major_triad")
+
+    note4 = get_starting_note(key, "IV")
+    chord4 = get_chord_second_inversion(key=key, note=note4, chord="major_triad")
+
+    return [chord1, chord2, chord3, chord4]
 
 
 def get_random_key():
